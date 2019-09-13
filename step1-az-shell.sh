d42f11cf24bccaaf8a417146487c636327bd99e9
honeybirdhk <39690993+honeybirdhk@users.noreply.github.com>
2019-09-13 12:26:57 +0800

    Update step1-az-shell.sh
    bug fix

--- step1-az-shell.sh
+++ step1-az-shell.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-myDockerGroup="peter-myDockerGroup-1"
+myResourceGroup="peter-myrecoursegroup-1"
 myDockerVM="peter-myDockerVM-1"
 
 az group create \
-	--name $myDockerGroup \
+	--name $myResourceGroup \
 	--location eastus
 
 echo "#include https://get/docker.com" > cloud-init.txt
 
 az vm create \
-	--resource-group $myDockerGroup \
+	--resource-group $myResourceGroup \
 	--name $myDockerVM \
 	--image UbuntuLTS \
 	--admin-username ierg4350vm \
@@ -18,12 +18,12 @@ az vm create \
 
 az vm open-port \
 	--port 80 \
-	--resource-group $myDockerGroup \
+	--resource-group $myResourceGroup \
 	--name $myDockerVM
 
 
 publicip=$(az vm list-ip-addresses \
-	-g $myDockerGroup \
+	-g $myResourceGroup \
 	-n $myDockerVM \
 	| grep ipAddress \
 	| tr ":" " " \
@@ -31,14 +31,14 @@ publicip=$(az vm list-ip-addresses \
 
 
 nsgname=$(az network nsg list \
-	--resource-group $myDockerGroup | \
+	--resource-group $myResourceGroup | \
 	grep networkSecurityGroup | grep id | head -1 | cut -d ":" -f2 | cut -d "/" -f9)
 
 az network nsg rule create \
 	--nsg-name $nsgname \
 	--name allow_8080 \
 	--priority 1010 \
-	--resource-group $myDockerGroup \
+	--resource-group $myResourceGroup \
 	--access Allow \
 	--destination-port-ranges 8080 \
 	--direction Inbound \
